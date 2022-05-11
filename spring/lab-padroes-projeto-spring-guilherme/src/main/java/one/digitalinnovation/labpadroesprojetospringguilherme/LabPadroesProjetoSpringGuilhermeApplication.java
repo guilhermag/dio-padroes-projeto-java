d350dd03c55487160c342580573ebe9b416f8e6e
Guilherme Gabriel <guilhermag@gmail.com>
2022-05-11 18:43:40 -0300

    arquivos iniciais projeto spring

---
+++ spring/lab-padroes-projeto-spring-guilherme/src/main/java/one/digitalinnovation/labpadroesprojetospringguilherme/LabPadroesProjetoSpringGuilhermeApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.labpadroesprojetospringguilherme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabPadroesProjetoSpringGuilhermeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPadroesProjetoSpringGuilhermeApplication.class, args);
+	}
+
+}
---
+++ spring/lab-padroes-projeto-spring-guilherme/src/test/java/one/digitalinnovation/labpadroesprojetospringguilherme/LabPadroesProjetoSpringGuilhermeApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.labpadroesprojetospringguilherme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabPadroesProjetoSpringGuilhermeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
